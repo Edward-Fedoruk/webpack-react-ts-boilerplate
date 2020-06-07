8659c6884f7c84f16834954c3cb460f34fd77d2a
Eduard Fedoruk <ed.fedorukk@gmail.com>
2020-06-08 00:57:22 +0200

    add redux store configurations

---
+++ src/store/configureStore.ts
@@ -0,0 +1,38 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { createInjectorsEnhancer, forceReducerReload } from 'redux-injectors';
+import createSagaMiddleware from 'redux-saga';
+import createReducer from './reducers';
+
+export default (initialState = {}) => {
+  const reduxSagaMonitorOptions = {};
+  const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
+  const { run: runSaga } = sagaMiddleware;
+
+  // sagaMiddleware: Makes redux-sagas work
+  const middlewares = [sagaMiddleware];
+
+  const enhancers = [
+    createInjectorsEnhancer({
+      createReducer,
+      runSaga,
+    }),
+  ];
+
+  const store = configureStore({
+    reducer: createReducer(),
+    middleware: [...getDefaultMiddleware(), ...middlewares],
+    preloadedState: initialState,
+    devTools: process.env.NODE_ENV !== 'production',
+    enhancers,
+  });
+
+  // Make reducers hot reloadable, see http://mxs.is/googmo
+  /* istanbul ignore next */
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      forceReducerReload(store);
+    });
+  }
+
+  return store;
+};
